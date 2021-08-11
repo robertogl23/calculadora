1f11e0fcaa9f3e6ead4bc580c60f30a6f55e6475
Roberto Carlos Galicia Lopez <dev.robertgl@gmail.com>
2021-08-11 18:03:34 -0500

    diseño v1

--- app/src/js/icons.js
+++ app/src/js/icons.js
@@ -1,17 +1,41 @@
 import inconDollarPath from '../images/icon-dollar.svg'
+import inconDollar2Path from '../images/icon-dollar-2.svg'
 import inconPersonPath from '../images/icon-person.svg'
 
-const iconPerson = new Image();
-iconPerson.src   = inconPersonPath;
-iconPerson.alt   = "icon-person"
+// const iconPerson = new Image();
+// iconPerson.src   = inconPersonPath;
+// iconPerson.alt   = "icon-person";
 
-Array.from( document.getElementsByName( "image-dollar" ) )?.map( ( icon ) => 
+const crearIcon = (opciones) => {
+    const icon = new Image();
+    icon.src   = opciones?.path;
+    icon.alt   = opciones?.alt;
+    opciones?.css && icon.classList.add(opciones.css)
+    document.getElementById( opciones?.id )?.appendChild( icon )
+    // return icon;
+}
+const ObtenerIconPorTipo = (type,id) => {
+    type === "person" && crearIcon({
+        path: inconPersonPath,
+        alt : "icon-person",
+        id  : id
+    })
+    type === "dollar" && crearIcon({
+        path: inconDollarPath,
+        alt : "icon-dollar",
+        id  : id
+    })
+    type === "dollar-2" && crearIcon({
+        path: inconDollar2Path,
+        alt : "icon-dollar",
+        id  : id,
+        css : "icon-dollar"
+    })
+}
+Array.from( document.getElementsByName( "icon" ) )?.map( ( icon ) => 
 {
-    const iconDollar = new Image();
-    iconDollar.src   = inconDollarPath;
-    iconDollar.alt   = "icon-dollar"
-    iconDollar.classList.add("icon-dollar")  
-    icon.appendChild( iconDollar )
+    icon.getAttribute("type") != null && ObtenerIconPorTipo(icon.getAttribute("type"),icon.id)
+
 })
     
-document.getElementById( "image-person" )?.appendChild( iconPerson )
+// document.getElementById( "image-person" )?.appendChild( iconPerson )
--- app/src/js/logo.js
+++ app/src/js/logo.js
@@ -3,5 +3,6 @@ import logoPath from '../images/logo.svg'
 const logo =  new Image()
 
 logo.src = logoPath;
+logo.alt = "logo";
 
 document.body.appendChild(logo)
