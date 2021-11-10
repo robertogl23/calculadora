3190302c777fee82c4b9488a26a755b13549f416
Roberto Carlos Galicia Lopez <dev.robertgl@gmail.com>
2021-11-09 23:20:06 -0600

    agregando logo al div con la classe logo

--- app/src/js/logo.js
+++ app/src/js/logo.js
@@ -1,8 +1,10 @@
 import logoPath from '../images/logo.svg'
+import { $ } from './app';
 
 const logo =  new Image()
 
 logo.src = logoPath;
 logo.alt = "logo";
+// logo.classList.add('logo')
 
-document.body.appendChild(logo)
+$('.logo').appendChild(logo)
